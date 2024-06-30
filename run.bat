5b552be34149cfeb267b79a814bed9033ae25865
yzs <1766891061@qq.com>
2024-06-30 22:59:22 +0800

    删除测试脚本

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set LOSS_RATIO=0.01
-REM 启动server.py
-start cmd /k "cd server && python server.py --loss_ratio=%LOSS_RATIO% --timeout=1 --win_size=4 --max_size=512"
-REM 等待一定时间，确保server.py已经启动
-timeout /t 1
-REM 启动client.py
-start cmd /k "cd client && python client.py --server_ip=127.0.0.1 --server_port=9999 --loss_ratio=%LOSS_RATIO% --timeout=1 --win_size=4 --max_size=512"
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# 启动server.py
-gnome-terminal --working-directory="$PWD/server" -e 'python server.py' &
-# 等待一定时间，确保server.py已经启动
-sleep 2
-# 启动client.py
-gnome-terminal --working-directory="$PWD/client" -e 'python client.py' &
-wait
